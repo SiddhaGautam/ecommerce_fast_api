27f00f501238613227f0976567711ab7dbe2f9ed
gautam tiwari <gautamtiwari0258@gmail.com>
2026-07-17 10:45:41 +0530

    completed post request

--- _change_post_.py
+++ _change_post_.py
@@ -5,4 +5,21 @@ from pydantic import BaseModel,Field
 app = FastAPI()
 
 class postClass(BaseModel):
-    name:str = Field()
+    name:str = Field(None,min_length=3)
+    age:int  = Field(None,gt=18,lt=70)
+    category:str = Field(None,description="category describes the field of the client(user)")
+
+data = []
+
+@app.post("/post/data")
+def post_data(user:postClass):
+    product = {
+        "name":user.name,
+        "age":user.age,
+        "category":user.category
+    }
+    data.append(product)
+    print(data)
+    return {
+        "data":user.model_dump(), "message":"created successfully"
+    }
